0a2f4da251e85ef9d2cdec71b02ef1693f4c1a12
Yujohn Nattrass <Yujohn.natt@gmail.com>
2025-05-29 13:32:42 -0700

    add back

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -16,32 +16,32 @@ export const mastra = new Mastra({
     name: 'Mastra',
     level: 'info',
   }),
-  // server: {
-  //   middleware: [
-  //     {
-  //       handler: async (c, next) => {
-  //       const isFromMastraCloud = c.req.header('x-mastra-cloud') === 'true';
-  //       const isDevPlayground = c.req.header('x-mastra-dev-playground') === 'true'
-  //       console.log('isFromMastraCloud', isFromMastraCloud);
-  //       console.log('isDevPlayground', isDevPlayground);
-  //       if(isFromMastraCloud || isDevPlayground) {
-  //         await next();
-  //         return;
-  //       }
+  server: {
+    middleware: [
+      {
+        handler: async (c, next) => {
+        const isFromMastraCloud = c.req.header('x-mastra-cloud') === 'true';
+        const isDevPlayground = c.req.header('x-mastra-dev-playground') === 'true'
+        console.log('isFromMastraCloud', isFromMastraCloud);
+        console.log('isDevPlayground', isDevPlayground);
+        if(isFromMastraCloud || isDevPlayground) {
+          await next();
+          return;
+        }
 
-  //       const authHeader = c.req.header("Authorization");
-  //       if (!authHeader || !authHeader.startsWith('Bearer ')) {
-  //         return new Response('Unauthorized', { status: 401 });
-  //       }
-  //       const token = authHeader.substring(7);
-  //       const validApiKey = process.env.BEARER_KEY || 'your-secret-api-key';
-  //       if (token !== validApiKey) {
-  //       return new Response('Invalid token', { status: 401 });
-  //       }
-  //       await next();
-  //       },
-  //       path: "/api/*",
-  //     },
-  //   ]
-  // }
+        const authHeader = c.req.header("Authorization");
+        if (!authHeader || !authHeader.startsWith('Bearer ')) {
+          return new Response('Unauthorized', { status: 401 });
+        }
+        const token = authHeader.substring(7);
+        const validApiKey = process.env.BEARER_KEY || 'your-secret-api-key';
+        if (token !== validApiKey) {
+        return new Response('Invalid token', { status: 401 });
+        }
+        await next();
+        },
+        path: "/api/*",
+      },
+    ]
+  }
 });
