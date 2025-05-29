48f9f78fdf2af332b418c2d4090e001f8795bdd9
Yujohn Nattrass <Yujohn.natt@gmail.com>
2025-05-29 09:49:25 -0700

    use openai

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -16,18 +16,6 @@ export const mastra = new Mastra({
     name: 'Mastra',
     level: 'info',
   }),
-  telemetry: {
-    serviceName: "ai",
-    enabled: true,
-    export: {
-      type: "custom",
-      exporter: new LangfuseExporter({
-        publicKey: process.env.LANGFUSE_PUBLIC_KEY,
-        secretKey: process.env.LANGFUSE_SECRET_KEY,
-        baseUrl: process.env.LANGFUSE_BASEURL,
-      }),
-    },
-  },
   server: {
     middleware: [
       {
